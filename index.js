0f8c0abf0a1bc386fc8b57781c5d028d95e86d1b
jinalfoflia <fofliajinal@gmail.com>
2017-01-05 14:50:25 +0530

    modified the script with more nav params

--- index.js
+++ index.js
@@ -40,6 +40,34 @@ stream.on('data', function (data) {
         tl_otherCount = tl_otherCount + 1;
        }
    }
+     if (tags.hasOwnProperty('building')) {
+        if (!(team.indexOf(data.user) < 0 )) {
+           bl_mapboxCount = bl_mapboxCount + 1;
+       } else {
+        bl_otherCount = bl_otherCount + 1;
+       }
+   }
+     if (tags.hasOwnProperty('noref') && tags.noref === 'yes') {
+        if (!(team.indexOf(data.user) < 0 )) {
+           nref_mapboxCount = nref_mapboxCount + 1;
+       } else {
+        nref_otherCount = nref_otherCount + 1;
+       }
+   }
+     if (tags.hasOwnProperty('ref')) {
+        if (!(team.indexOf(data.user) < 0 )) {
+           ref_mapboxCount = ref_mapboxCount + 1;
+       } else {
+        ref_otherCount = ref_otherCount + 1;
+       }
+   }
+     if (tags.hasOwnProperty('destination')) {
+        if (!(team.indexOf(data.user) < 0 )) {
+           des_mapboxCount = des_mapboxCount + 1;
+       } else {
+        des_otherCount = des_otherCount + 1;
+       }
+   }
 });
 
 stream.on('end', function() {
@@ -47,4 +75,12 @@ stream.on('end', function() {
     process.stderr.write('TR by Others: ' + String(tr_otherCount) + '\n');
     process.stderr.write('TL by Mapbox: ' + String(tl_mapboxCount) + '\n');
     process.stderr.write('TL by Others: ' + String(tl_otherCount) + '\n');
+    process.stderr.write('TL by Mapbox: ' + String(bl_mapboxCount) + '\n');
+    process.stderr.write('TL by Others: ' + String(bl_otherCount) + '\n');
+    process.stderr.write('TL by Mapbox: ' + String(nref_mapboxCount) + '\n');
+    process.stderr.write('TL by Others: ' + String(nref_otherCount) + '\n');
+    process.stderr.write('TL by Mapbox: ' + String(ref_mapboxCount) + '\n');
+    process.stderr.write('TL by Others: ' + String(ref_otherCount) + '\n');
+    process.stderr.write('TL by Mapbox: ' + String(des_mapboxCount) + '\n');
+    process.stderr.write('TL by Others: ' + String(des_otherCount) + '\n');
 });
