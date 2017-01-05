f907d70636a1304771a6e8b0b417adad55f4c18c
jinalfoflia <fofliajinal@gmail.com>
2017-01-05 14:08:29 +0530

    modified the script with more nav params

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ var team = require('mapbox-data-team').getUsernames();
 
 var file = new osmium.File(argv.file);
 var reader = new osmium.Reader(file);
-var stream = new osmium.Stream(new osmium.Reader(file, {relation: true, node: false, ways: false}));
+var stream = new osmium.Stream(new osmium.Reader(file, {relation: true, node: true, ways: true}));
 
 var tr_mapboxCount = 0;
 var tr_otherCount = 0;
+var bl_mapboxCount = 0;
+var bl_otherCount = 0;
+var ref_mapboxCount = 0;
+var ref_otherCount = 0;
+var nref_mapboxCount = 0;
+var nref_otherCount = 0;  
+var des_mapboxCount = 0;
+var des_otherCount = 0;
+var tl_mapboxCount = 0;
+var tl_otherCount = 0;
+
+
 
 stream.on('data', function (data) {
     var f;
@@ -21,9 +33,18 @@ stream.on('data', function (data) {
         tr_otherCount = tr_otherCount + 1;
        }
    }
+   if (tags.hasOwnProperty('turn:lanes')) {
+        if (!(team.indexOf(data.user) < 0 )) {
+           tl_mapboxCount = tl_mapboxCount + 1;
+       } else {
+        tl_otherCount = tl_otherCount + 1;
+       }
+   }
 });
 
 stream.on('end', function() {
     process.stderr.write('TR by Mapbox: ' + String(tr_mapboxCount) + '\n');
     process.stderr.write('TR by Others: ' + String(tr_otherCount) + '\n');
+    process.stderr.write('TL by Mapbox: ' + String(tl_mapboxCount) + '\n');
+    process.stderr.write('TL by Others: ' + String(tl_otherCount) + '\n');
 });
